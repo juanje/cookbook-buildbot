9338388d4f88d90bd0b7e23022e7a4d340568be7
Juanje Ojeda <juanje.ojeda@gmail.com>
2012-08-14 07:07:29 +0100

    Some refactoring with attributes and spliting between master and slaves conf

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,19 +1,34 @@
-default['buildbot']['packages'] = %w{ git python-dev }
-default['buildbot']['pip_packages'] = %w{ buildbot buildbot-slave }
-
 default['buildbot']['user'] = 'buildbot'
 default['buildbot']['group'] = 'buildbot'
 
-default['buildbot']['deplot_to'] = '/opt/buildbot'
-default['buildbot']['master_dir'] = 'master'
-default['buildbot']['slave_dir'] = 'slave'
+default['buildbot']['master']['packages'] = %w{ git python-dev }
+default['buildbot']['master']['pip_packages'] = %w{ buildbot }
+default['buildbot']['master']['deploy_to'] = '/opt/buildbot'
+default['buildbot']['master']['basedir'] = 'master'
+default['buildbot']['master']['options'] = ''
+default['buildbot']['master']['cfg'] = ::File.join(
+  node['buildbot']['master']['deploy_to'],
+  node['buildbot']['master']['basedir'],
+  'master.cfg')
 
+
+default['buildbot']['slave']['packages'] = %w{ git python-dev }
+default['buildbot']['slave']['pip_packages'] = %w{ buildbot-slave }
+default['buildbot']['slave']['deploy_to'] = '/opt/buildbot'
+default['buildbot']['slave']['options'] = ''
+default['buildbot']['slaves'] = [{
+  :name     => 'example-slave',
+  :password => 'pass',
+  :basedir  => 'slave'
+}]
 default['buildbot']['slave']['name'] = 'example-slave'
 default['buildbot']['slave']['password'] = 'pass'
+default['buildbot']['slave']['basedir'] = 'slave'
 
 default['buildbot']['repo']['uri'] = 'git://github.com/buildbot/pyflakes.git'
 default['buildbot']['repo']['branch'] = 'master'
 default['buildbot']['repo']['workdir'] = 'gitpoller-workdir'
 
+default['buildbot']['status']['force_build'] = 'True' # 'False' or 'auth'
 default['buildbot']['status']['auth_user'] = 'pyflakes'
 default['buildbot']['status']['auth_pass'] = 'pyflakes'
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,10 +1,14 @@
 include_recipe "python::pip"
 
-node['buildbot']['packages'].each do |pkg|
+# Master
+
+master = node['buildbot']['master']
+
+master['packages'].each do |pkg|
   package pkg
 end
 
-node['buildbot']['pip_packages'].each do |pkg|
+master['pip_packages'].each do |pkg|
   python_pip pkg do
     action :install
   end
@@ -16,7 +20,7 @@ user "buildbot" do
   shell "/bin/false"
 end
 
-directory node['buildbot']['deplot_to'] do
+directory master['deploy_to'] do
   owner node['buildbot']['user']
   group node['buildbot']['group']
   mode "0755"
@@ -24,8 +28,8 @@ directory node['buildbot']['deplot_to'] do
 end
 
 execute "Create master" do
-  command "buildbot create-master #{node['buildbot']['master_dir']}"
-  cwd node['buildbot']['deplot_to']
+  command "buildbot create-master #{master['options']} #{master['basedir']}"
+  cwd master['deploy_to']
   user node['buildbot']['user']
   group node['buildbot']['group']
   action :run
@@ -33,42 +37,69 @@ end
 
 execute "Start the master" do
   command "buildbot start master"
-  cwd node['buildbot']['deplot_to']
+  cwd master['deploy_to']
   user node['buildbot']['user']
   group node['buildbot']['group']
   action :nothing
 end
 
-master_cfg = ::File.join(node['buildbot']['deplot_to'],
-                         node['buildbot']['master_dir'],
-                         'master.cfg')
-template master_cfg do
+force_build_attrib = node['buildbot']['status']['force_build']
+force_build = case force_build_attrib
+              when /^[Tt]rue$/, /^[Ff]alse$/
+                force_build_attrib.capitalize
+              else
+                %{'#{force_build_attrib}'}
+              end
+
+template master['cfg'] do
   source "master.cfg.erb"
   owner node['buildbot']['user']
   group node['buildbot']['group']
   variables(
-    :slave_name => node['buildbot']['slave']['name'],
-    :slave_pass => node['buildbot']['slave']['password'],
-    :repo => node['buildbot']['repo']['uri'],
-    :branch => node['buildbot']['repo']['branch'],
-    :workdir => node['buildbot']['repo']['workdir'],
-    :auth_user => node['buildbot']['status']['auth_user'],
-    :auth_pass => node['buildbot']['status']['auth_pass']
+    :slaves      => node['buildbot']['slaves'],
+    :repo        => node['buildbot']['repo']['uri'],
+    :branch      => node['buildbot']['repo']['branch'],
+    :workdir     => node['buildbot']['repo']['workdir'],
+    :force_build => force_build,
+    :auth_user   => node['buildbot']['status']['auth_user'],
+    :auth_pass   => node['buildbot']['status']['auth_pass']
   )
   notifies :run, resources(:execute => "Start the master"), :immediately
 end
 
+
+# Slave
+
+slave = node['buildbot']['slave']
+
+slave['packages'].each do |pkg|
+  package pkg
+end
+
+slave['pip_packages'].each do |pkg|
+  python_pip pkg do
+    action :install
+  end
+end
+
+directory master['deploy_to'] do
+  owner node['buildbot']['user']
+  group node['buildbot']['group']
+  mode "0755"
+  action :create
+end
+
 execute "Start the slave" do
   command "buildslave start slave"
-  cwd node['buildbot']['deplot_to']
+  cwd slave['deploy_to']
   user node['buildbot']['user']
   group node['buildbot']['group']
   action :nothing
 end
 
 execute "Create slave" do
-  command "buildslave create-slave #{node['buildbot']['slave_dir']} localhost:9989 #{node['buildbot']['slave']['name']} #{node['buildbot']['slave']['password']}"
-  cwd node['buildbot']['deplot_to']
+  command "buildslave create-slave #{slave['options']} #{slave['basedir']} localhost:9989 #{slave['name']} #{slave['password']}"
+  cwd slave['deploy_to']
   user node['buildbot']['user']
   group node['buildbot']['group']
   notifies :run, resources(:execute => "Start the slave")
