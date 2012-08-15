d7d0663e4034e2a931dc8a6ec76151c40d013760
Juanje Ojeda <juanje.ojeda@gmail.com>
2012-08-15 04:11:16 +0100

    Split the default recipe into master and slaves ones

---
+++ recipes/master.rb
@@ -0,0 +1,71 @@
+include_recipe "python::pip"
+
+master = node['buildbot']['master']
+
+master['packages'].each do |pkg|
+  package pkg
+end
+
+master['pip_packages'].each do |pkg|
+  python_pip pkg do
+    action :install
+  end
+end
+
+user "buildbot" do
+  comment "Buildbot user"
+  system true
+  shell "/bin/false"
+end
+
+directory master['deploy_to'] do
+  owner node['buildbot']['user']
+  group node['buildbot']['group']
+  mode "0755"
+  action :create
+end
+
+execute "Create master" do
+  command "buildbot create-master #{master['options']} #{master['basedir']}"
+  cwd master['deploy_to']
+  user node['buildbot']['user']
+  group node['buildbot']['group']
+  action :run
+end
+
+execute "Start the master" do
+  command "buildbot restart master"
+  cwd master['deploy_to']
+  user node['buildbot']['user']
+  group node['buildbot']['group']
+  action :nothing
+end
+
+force_build_attrib = node['buildbot']['status']['force_build']
+force_build = case force_build_attrib
+              when /^[Tt]rue$/, /^[Ff]alse$/
+                force_build_attrib.capitalize
+              else
+                %{'#{force_build_attrib}'}
+              end
+
+template master['cfg'] do
+  source "master.cfg.erb"
+  owner node['buildbot']['user']
+  group node['buildbot']['group']
+  variables(
+    :slaves      => node['buildbot']['slaves'],
+    :title       => node['buildbot']['project']['title'],
+    :title_url   => node['buildbot']['project']['title_url'],
+    :repo        => node['buildbot']['project']['repo'],
+    :branch      => node['buildbot']['project']['branch'],
+    :workdir     => node['buildbot']['project']['workdir'],
+    :steps       => node['buildbot']['steps'],
+    :force_build => force_build,
+    :auth_user   => node['buildbot']['status']['auth_user'],
+    :auth_pass   => node['buildbot']['status']['auth_pass']
+  )
+  notifies :run, resources(:execute => "Start the master"), :immediately
+end
+
+
---
+++ recipes/slaves.rb
@@ -0,0 +1,37 @@
+include_recipe "python::pip"
+
+slave = node['buildbot']['slave']
+
+slave['packages'].each do |pkg|
+  package pkg
+end
+
+slave['pip_packages'].each do |pkg|
+  python_pip pkg do
+    action :install
+  end
+end
+
+directory slave['deploy_to'] do
+  owner node['buildbot']['user']
+  group node['buildbot']['group']
+  mode "0755"
+  action :create
+end
+
+execute "Start the slave" do
+  command "buildslave restart slave"
+  cwd slave['deploy_to']
+  user node['buildbot']['user']
+  group node['buildbot']['group']
+  action :nothing
+end
+
+execute "Create slave" do
+  command "buildslave create-slave #{slave['options']} #{slave['basedir']} localhost:9989 #{slave['name']} #{slave['password']}"
+  cwd slave['deploy_to']
+  user node['buildbot']['user']
+  group node['buildbot']['group']
+  notifies :run, resources(:execute => "Start the slave")
+end
+
