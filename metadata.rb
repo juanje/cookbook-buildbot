0bf759fad351ca655b739e4eeb0b500d8bebd2c7
Juanje Ojeda <juanje.ojeda@gmail.com>
2012-08-16 07:01:08 +0100

    Add the first metadata.rb version

---
+++ metadata.rb
@@ -0,0 +1,16 @@
+maintainer       "Juanje Ojeda"
+maintainer_email "juanje.ojeda@gmail.com"
+license          "Apache 2.0"
+description      "Installs and configures a Buildbot master and/or slave"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+recipe           "buildbot", "Install a master and slave Buildbot with the default configuration"
+recipe           "buildbot::master", "Install and configure a Buildbot master"
+recipe           "buildbot::slave", "Install and configure a Buildbot slave"
+
+depends "python"
+
+%w{redhat centos scientific fedora debian ubuntu}.each do |os|
+    supports os
+end
+
